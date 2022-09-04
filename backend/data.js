8f80cbbcee3c5be82047c6f5061c1a98783bfff8
ugochukwuomeje <ugochukwuomeje1@gmail.com>
2022-09-04 14:48:18 +0100

    create both fron end and backend

---
+++ backend/data.js
@@ -0,0 +1,72 @@
+import bcrypt from "bcryptjs";
+const data = {
+  users: [
+    {
+      name: "Emeka",
+      email: "emeka@example.com",
+      password: bcrypt.hashSync("123456"),
+      isAdmin: true,
+    },
+    {
+      name: "Ugochukwu",
+      email: "ugochukwu@example.com",
+      password: bcrypt.hashSync("123456"),
+      isAdmin: false,
+    },
+  ],
+  products: [
+    {
+      // _id: "1",
+      name: "Nike Slim shirt",
+      slug: "nike-slim-shirt",
+      category: "Shirts",
+      image: "/images/p1.jpeg", // 679px × 829px
+      price: 120,
+      countInStock: 10,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality shirt",
+    },
+    {
+      // _id: "2",
+      name: "Adidas Fit Shirt",
+      slug: "adidas-fit-shirt",
+      category: "Shirts",
+      image: "/images/p2.jpeg",
+      price: 250,
+      countInStock: 20,
+      brand: "Adidas",
+      rating: 4.0,
+      numReviews: 10,
+      description: "high quality product",
+    },
+    {
+      // _id: "3",
+      name: "Nike Slim Pant",
+      slug: "nike-slim-pant",
+      category: "Pants",
+      image: "/images/p3.jpeg",
+      price: 25,
+      countInStock: 15,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 14,
+      description: "high quality product",
+    },
+    {
+      // _id: "4",
+      name: "Adidas Fit Pant",
+      slug: "adidas-fit-pant",
+      category: "Pants",
+      image: "/images/p4.jpeg",
+      price: 65,
+      countInStock: 5,
+      brand: "Puma",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality product",
+    },
+  ],
+};
+export default data;
